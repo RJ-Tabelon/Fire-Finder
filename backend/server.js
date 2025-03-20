a9c8ca017cc2b6315239a1c1f313f83e462668f5
Granache <dcfallon04@gmail.com>
2025-03-20 19:28:02 -0400

    implemented basics of express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
 // made by Dia Fallon on 3/19
+// This will function as the main file for the backend of the project
+
+// import and export syntax available from "type": "module", in package.json
+import express from 'express';
+
+const app = express();
+
+// To run this through terminal, cd into backend folder and then enter
+// node .\backend\server.js
+app.listen(5001, () => {
+    console.log('Server started at http://localhost:5001');
+});
