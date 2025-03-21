21e56c4cbeef930afda5ca7c1a11ba718ac05601
Granache <dcfallon04@gmail.com>
2025-03-20 21:28:03 -0400

    implemented app.get

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 // made by Dia Fallon on 3/19
 // This will function as the main file for the backend of the project
 
+// To run this through terminal, cd into backend folder and then enter
+// node .\backend\server.js
+// or
+// npm run dev
+
 // import and export syntax available from "type": "module", in package.json
 import express from 'express';
 
 const app = express();
 
-// To run this through terminal, cd into backend folder and then enter
-// node .\backend\server.js
-// or
-// npm run dev
+app.get("/", (req,res) => {
+    res.send("Server is ready");
+});
 
 app.listen(5001, () => {
     console.log('Server started at http://localhost:5001');
