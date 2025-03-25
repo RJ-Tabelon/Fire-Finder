c5c8e808be7dcee31e619dc51be31371c4f80667
Granache <dcfallon04@gmail.com>
2025-03-25 18:05:58 -0400

    fixed "cannot GET /" error from lack of homepage

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.get("/", (req,res) => {
+    res.send("Server is ready");
+});
+
 app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
